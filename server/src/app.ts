8f3cdd18b68454e44e504270af4a18f63f7ce75e
TheAdri666 <adrianinfante66@gmail.com>
2023-04-25 00:23:48 +0300

    Finished project

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,9 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
-import userRouter from './routes/userRouter';
-import codeSnippetRouter from './routes/codeSnippetRouter';
-import cors from 'cors';
-import path from 'path';
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRouter";
+import codeSnippetRouter from "./routes/codeSnippetRouter";
+import cors from "cors";
+import path from "path";
 
 const app: Express = express();
 
@@ -15,17 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Use the custom-made routers.
-app.use('/api/user', userRouter);
-app.use('/api/codeSnippet', codeSnippetRouter);
+app.use("/api/user", userRouter);
+app.use("/api/codeSnippet", codeSnippetRouter);
 
 // Make the server work in both production and development modes
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve("..", "client", "build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve("..", "client", "build", "index.html"))); 
-} else if (process.env.NODE_ENV === "development") { 
-  const corsOptions = { 
-    origin: "http://localhost:3000", 
-    optionsSuccessStatus: 200,  
+  app.use(express.static(path.resolve("..", "client", "build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("..", "client", "dist", "client", "index.html"))
+  );
+} else if (process.env.NODE_ENV === "development") {
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
   };
   app.use(cors(corsOptions));
 }
